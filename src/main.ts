a84c450439996209ebe74bd20c924602cf7e5cbb
wangshangwen <shangwenwang20@gmail.com>
2023-01-19 19:10:38 +0800

    feat: add more linter components

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import ms from "ms";
-import lunchtime from "./lunchtime";
-import millisecondsUntil from "./millisecondsUntil";
+import ms from 'ms'
+import lunchtime from './lunchtime'
+import millisecondsUntil from './millisecondsUntil'
 
-function howLongUntilLunch(hours: number = 12, minutes: number = 30): string {
+function howLongUntilLunch(hours = 12, minutes = 30): string {
   const millisecondsUntilLunchTime = millisecondsUntil(
     lunchtime(hours, minutes)
-  );
-  const result = ms(millisecondsUntilLunchTime, { long: true });
-  console.log(result);
-  return result;
+  )
+  const result = ms(millisecondsUntilLunchTime, { long: true })
+  console.log(result)
+  return result
 }
 
-howLongUntilLunch();
+howLongUntilLunch()
