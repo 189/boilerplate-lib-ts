b752939243932d7bcbc72f4a22f50436ae2be0dc
wangshangwen <shangwenwang20@gmail.com>
2023-01-20 21:43:55 +0800

    refactor: refactor

---
+++ index.js
@@ -0,0 +1,15 @@
+const questions = {
+  name: {
+    desc: 'project name',
+  },
+  description: {
+    desc: 'project description',
+  },
+  author: {
+    desc: 'project author',
+  },
+}
+
+module.exports = {
+  questions,
+}
